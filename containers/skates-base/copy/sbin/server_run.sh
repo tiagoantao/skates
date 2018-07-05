9e148af609763bdc24b0014cc36d4abb52b5984c
Tiago Antao <tiagoantao@gmail.com>
2018-07-05 15:55:59 -0600

    Removing supervisord by default

--- containers/skates-base/copy/sbin/server_run.sh
+++ containers/skates-base/copy/sbin/server_run.sh
@@ -2,18 +2,21 @@
 
 /sbin/link_mutable_files.py
 
-if [ -e /sbin/prepare_magic.sh ]
+if [ -e /sbin/prepare_magic.sh ];
 then
     bash /sbin/prepare_magic.sh
 fi
 
 #in the (probable) case /var/log is a VOLUME
 if [ ! -e /var/log/supervisor ]; then
-   mkdir /var/log/supervisor
-   #a few extras
-   mkdir /var/log/nginx
-   mkdir /var/log/zabbix
-   mkdir /var/log/uwsgi
+    mkdir /var/log/supervisor
+    #a few extras, should not be here
+    mkdir /var/log/nginx
+    mkdir /var/log/zabbix
+    mkdir /var/log/uwsgi
 fi
 
-/usr/bin/supervisord
+if [ -v WITH_SUPERVISOR ];
+then
+    /usr/bin/supervisord
+fi
